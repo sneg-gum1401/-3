41083a61dd6b04169156c74481f2f06d920ec746
sneg-gum1401 <sneg.gum@gmail.com>
2025-04-04 12:09:53 +0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-char colors[3][10] = {"red", "blue", "green"};
+char colors[3][10] = {"красный", "синий", "зеленый"};
 
-strcpy(colors[0], "yellow");
+strcpy(colors[0], "желтый");
 
 for (int i = 0; i <= 3; i++)
 {
